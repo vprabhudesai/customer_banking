0349a07b24d45d8d348d73d006ad33e0e3bd9ef6
vprabhudesai <vishesh.prabhudesai@gmail.com>
2024-10-03 20:19:21 -0400

    first

--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 
 # Define the main function
 def main():
@@ -9,21 +11,35 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
+    savings_balance = float(input("Enter the savings balance: "))
+
+    savings_interest = float(input("Enter the savings interest rate: "))
+
+    savings_maturity = int(input("Enter the savings account length (in months): "))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"The updated savings account balance is ${updated_savings_balance:,.2f} with ${interest_earned:,.2f} interest earned.")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance = float(input("Enter the CD balance: "))
 
+    cd_interest = float(input("Enter the CD interest rate: "))
+
+    cd_maturity = int(input("Enter the CD account length (in months): "))
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print(f"The updated CD account balance is ${updated_cd_balance:,.2f} with ${interest_earned} interest earned.")
+
 
 if __name__ == "__main__":
+    
     # Call the main function.
+    main()
